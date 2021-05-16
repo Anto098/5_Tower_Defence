6b00a79b5181e47c6fb380ff1682d2488eead9f3
Antonin <antonin.roy1@gmail.com>
2021-05-16 14:20:42 -0400

    grid system

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x,
+            0f,
+            waypoint.GetGridPos().y     // waypoint is a 2d Vector so the 2nd parameter is the z
+        );                              // in the 3d world, but it's y in our 2d vector
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        string labelText = transform.position.x / 10 + "," + transform.position.z / 10; // le prof a fait : waypoint.GetGridPos().x au lieu de transform.position.x
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( FollowPath() );
+        print("Hey I'm back at start!");
+    }
+
+    private IEnumerator FollowPath()
+    {
+        print("Starting patrol.");
+        foreach (Waypoint w in path)
+        {
+            transform.position = new Vector3(w.transform.position.x, 4, w.transform.position.z);
+            print("Visiting block: "+w.transform.position.x/10f+","+w.transform.position.z/10f);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
